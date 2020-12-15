440ed8be7ca40f92f401751afb3d0dd1aa3bf4ce
Yusufbek Alimatov <youbekalimatov@gmail.com>
2020-12-16 01:08:34 +0500

    setNewImage function to read static files and append them to workbench.html Added template.js to update workbench.html file

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,77 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
+import * as upath from "upath";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "background-image" is now active!'
+  setNewImage(
+    "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80"
   );
 
-  let disposable = vscode.commands.registerCommand(
-    "background-image.helloWorldHiThere",
-    () => {
-      vscode.window.showErrorMessage(
-        "Hello World from background-image!, This is my first VSCode Extension!"
-      );
-    }
+  const disposable = vscode.commands.registerCommand(
+    "extension.setBackgroundImage",
+    setNewImage
   );
 
   context.subscriptions.push(disposable);
+}
+
+function setNewImage(image: string) {
+  if (!require.main) {
+    vscode.window.showErrorMessage(
+      "Background.updateCSS: require.main is undefined!"
+    );
+    return;
+  }
+
+  const htmlFile = upath.join("/electron-browser/workbench/workbench.html");
+
+  try {
+    const cssStyles = fs
+      .readFileSync(upath.resolve(__dirname, "./static/style.css"), "utf-8")
+      .replace(/\[BACKGROUND_IMAGE\]/g, image);
+    const jsTemplate = fs.readFileSync(
+      upath.resolve(__dirname, "./static/template.js"),
+      "utf-8"
+    );
+    const jsTemplateWithCSS = jsTemplate.replace(
+      /\[BACKGROUND_STYLES\]/g,
+      cssStyles
+    );
+
+    const html = fs.readFileSync(htmlFile, "utf-8");
+
+    // add script tag
+    let output = html.replace(
+      /\<\/html\>/g,
+      `<script>${jsTemplateWithCSS}</script>\n`
+    );
+    output += "</html>";
+
+    fs.writeFileSync(htmlFile, output, "utf-8");
+
+    vscode.window
+      .showInformationMessage(
+        "Background images enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.",
+        { title: "Restart editor to complete" }
+      )
+      .then(function () {
+        vscode.commands.executeCommand("workbench.action.reloadWindow");
+      });
+  } catch (e) {
+    vscode.window.showErrorMessage(e.message);
+
+    // if (/ENOENT|EACCES|EPERM/.test(e.code)) {
+    //   vscode.window.showInformationMessage(
+    //     "You must run VS code with admin priviliges in order to enable Neon Dreams."
+    //   );
+    //   return;
+    // } else {
+    //   vscode.window.showErrorMessage(
+    //     "Something went wrong when starting neon dreams"
+    //   );
+    //   return;
+    // }
+  }
 }
 
 // this method is called when your extension is deactivated
---
+++ src/static/template.js
@@ -0,0 +1,55 @@
+(function () {
+  const bodyNode = document.querySelector("body");
+ 
+  function initialize(obs) {
+    const themeStyleTag = document.querySelector(".vscode-tokens-styles");
+
+    if(!themeStyleTag) {
+      return;
+    }
+  
+    const initialThemeStyles = themeStyleTag.innerText;
+    const updatedThemeStyles = `${initialThemeStyles}[BACKGROUND_STYLES]`;
+  
+    const newStyleTag = document.createElement("style");
+    newStyleTag.setAttribute("id", "background-image");
+    newStyleTag.innerText = updatedThemeStyles.replace(/(\r\n|\n|\r)/gm, '');
+    document.body.appendChild(newStyleTag);
+  
+    console.log(`Background Image initialised!`);
+  
+    if(obs) {
+      obs.disconnect();
+    }
+  }
+
+  // Callback function to execute when mutations are observed
+  function watchForInitialize(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'attributes') {
+          const tokensLoaded = document.querySelector('.vscode-tokens-styles');
+
+          // sometimes VS code takes a while to init the styles content, so stop this observer and add an observer for that
+          if (tokensLoaded) {
+            observer.disconnect();
+            observer.observe(tokensLoaded, { childList: true });
+          }
+        }
+
+        if (mutation.type === 'childList') {
+          const tokensLoaded = document.querySelector('.vscode-tokens-styles');
+          const tokenStyles = document.querySelector('.vscode-tokens-styles').innerText;
+
+          // Everything we need is ready, so initialise
+          if (tokensLoaded && tokenStyles) {
+            initialize(observer);
+          }
+        }
+    }
+  };
+
+  initialize();
+
+  const observer = new MutationObserver();
+  observer.observe(bodyNode, { attributes: true });
+})();
